c65b9a8677bf468d2297958b049532fccd3f90b9
Kanmii <maneptha@gmail.com>
2021-06-08 08:54:42 +0200

    run.sh:use unison file sync utility

--- run.fish
+++ run.fish
@@ -27,3 +27,36 @@ function vscode_sync
     $vimfolder_src/vscode.vim \
     $vimfolder_destination
 end
+
+set line '============='
+
+function sync_unison
+  if test -z "$winuser"
+    echo -e "\nwinuser must be set. Exiting."
+    return
+  end
+
+  for filename in 'settings.json' 'snippets' 'keybindings.json'
+    set source "./user-name/AppData/Roaming/Code/User/$filename"
+    echo -e "$line Syncing $source $line\n\n"
+
+    unison \
+      "$source" \
+      "/c/Users/$winuser/AppData/Roaming/Code/User/$filename" \
+      -fat
+
+    echo -e "\n\n$line *DONE* syncing $source $line\n\n"
+  end
+
+  for filename in 'lua' 'init.vin' 'regular.vim' 'vscode.vim'
+    set source "./user-name/AppData/Local/nvim/$filename"
+    echo -e "$line Syncing $source $line\n\n"
+
+    unison \
+      "$source" \
+      "/c/Users/$winuser/AppData/Local/nvim/$filename" \
+    -fat
+
+    echo -e "\n\n$line *DONE* syncing $source $line\n\n"
+  end
+end
